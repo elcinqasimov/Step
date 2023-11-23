ba78b50a3cdc2108340745c22a09df7069bb83be
Elcin Qasimov <elcinqasimov@gmail.com>
2023-11-24 00:12:01 +0400

    Update camp_add.php

--- admin/camp_add.php
+++ admin/camp_add.php
@@ -149,7 +149,7 @@ if(isset($_GET["mod"]) && $_GET["mod"] == "edit"){
 	$enddate	=	tarix($regsiyahi[0]["enddate"]);
 	$desc_az	=	$regsiyahi[0]["description_az"];
 	$desc_en	=	$regsiyahi[0]["description_en"];
-}elseif(isset($_POST["submit"]) && ($_POST["submit"] == "add")){
+}elseif(isset($_POST["submit"]) && $_POST["submit"] == "add"){
 	$camp_id	=	$_POST["camp_id"];
 	$name		=	$_POST["name"];
 	$count		=	$_POST["count"];
@@ -174,7 +174,7 @@ if(isset($_GET["mod"]) && $_GET["mod"] == "edit"){
         document.getElementById("camp_id").options.selectedIndex = <?=$regsiyahi[0]["camp_id"]?>;
     };
 </script>
-<?php }elseif(isset($_POST["submit"]) && ($_POST["submit"] == "add")){?>
+<?php }elseif(isset($_POST["submit"]) && $_POST["submit"] == "add"){?>
 	<script type="text/javascript">
 	window.onload = function(event) {
         document.getElementById("camp_id").options.selectedIndex = <?=$_POST["camp_id"]?>;
